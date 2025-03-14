0002d3d74cb6d5c776101c98f881d82173ec1a8d
Long-Zixuan <778275146@qq.com>
2025-03-14 13:13:01 +0800

    进一步完善了翻译“选项”的逻辑   LZX-2025-03-14-002

--- src/main/java/loongly/sclp/mixin/sodium/MixinAbstractWidget.java
+++ src/main/java/loongly/sclp/mixin/sodium/MixinAbstractWidget.java
@@ -14,9 +14,17 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.client.util.math.MatrixStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(AbstractWidget.class)
 public class MixinAbstractWidget
 {
+    private boolean isNum(String str) {
+        return str != null && str.matches("-?\\d+(\\.\\d+)?");
+    }
+
     @Final
     @Shadow
     protected TextRenderer font;
@@ -27,10 +35,41 @@ public class MixinAbstractWidget
         {
             String[] tmp = text.split(" ");
             StringBuilder textBuilder = new StringBuilder();
+            List<String> transList = new ArrayList<>();
+            String transStr;
+            //boolean isNum = false;
             for(String t : tmp)
             {
+                if(isNum(t))
+                {
+                    if(textBuilder.length() > 0)
+                    {
+                        transStr = textBuilder.toString();
+                        transList.add(transStr.substring(0,transStr.length() - 1));
+                        textBuilder = new StringBuilder();
+                        transList.add(" " + t + " ");
+                    }
+                    else
+                    {
+                        transList.add(t + " ");
+                    }
+
+                }
+                else
+                {
+                    textBuilder.append(t).append(" ");
+                }
+            }
+            if(textBuilder.length() > 0)
+            {
+                transStr = textBuilder.toString();
+                transList.add(transStr.substring(0, transStr.length() - 1));
+            }
+            textBuilder = new StringBuilder();
+            for(String t : transList)
+            {
                 t = I18n.translate(t);
-                textBuilder.append(t).append(" ");
+                textBuilder.append(t);
             }
             text = textBuilder.toString();
             //text = I18n.translate(text);
