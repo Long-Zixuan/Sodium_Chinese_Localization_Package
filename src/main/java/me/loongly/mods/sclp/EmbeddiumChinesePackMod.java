f89afa2e2733cd4e46065ad230d4bcd485a24e72
Long-Zixuan <778275146@qq.com>
2025-07-27 22:27:24 +0800

    First Commit   LZX-2025-07-27-011

---
+++ src/main/java/me/loongly/mods/sclp/EmbeddiumChinesePackMod.java
@@ -0,0 +1,13 @@
+package me.loongly.mods.sclp;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EmbeddiumChinesePackMod.MOD_ID)
+public final class EmbeddiumChinesePackMod {
+    public static final String MOD_ID = "sclp";
+
+    public EmbeddiumChinesePackMod()
+    {
+
+    }
+}
