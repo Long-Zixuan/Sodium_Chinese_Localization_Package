be53efac4c7e3cd15928fc215c8523bfb5395e76
Long-Zixuan <778275146@qq.com>
2025-08-10 00:57:18 +0800

    First Commit   LZX-git-2025-08-10-006

---
+++ src/main/java/me/loongly/mods/sclp/client/SCLPClientMod.java
@@ -0,0 +1,16 @@
+package me.loongly.mods.sclp.client;
+
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod("sclp")
+@OnlyIn(Dist.CLIENT)
+public class SCLPClientMod
+{
+
+}
