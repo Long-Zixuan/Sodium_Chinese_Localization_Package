3e4dceb2924b37000e00d1be066dc2be71e756fb
Long-Zixuan <778275146@qq.com>
2025-12-31 12:28:33 +0800

    First Commit   LZX-2025-12-31-001

---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    id("java")
+    id("fabric-loom") version ("1.14-SNAPSHOT") apply (false)
+}
+
+val MINECRAFT_VERSION by extra { "1.21.11" }
+val NEOFORGE_VERSION by extra { "21.11.10-beta" }
+val FABRIC_LOADER_VERSION by extra { "0.18.2" }
+val FABRIC_API_VERSION by extra { "0.139.4+1.21.11" }
+
+// This value can be set to null to disable Parchment.
+val PARCHMENT_VERSION by extra { null }
+
+// https://semver.org/
+val MAVEN_GROUP by extra { "me.loongly.mods" }
+val ARCHIVE_NAME by extra { "sclp" }
+val MOD_VERSION by extra { "4.0.0" }
+val SODIUM_VERSION by extra { "0.8.1+mc1.21.11" }
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+    group = MAVEN_GROUP
+    version = createVersionString()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+subprojects {
+    apply(plugin = "maven-publish")
+
+    repositories {
+        maven("https://maven.parchmentmc.org/")
+        maven("https://api.modrinth.com/maven")
+        maven("https://libraries.minecraft.net/")
+        maven("https://maven.caffeinemc.net/releases")
+        maven("https://maven.caffeinemc.net/snapshots")
+    }
+
+    base {
+        archivesName = "$ARCHIVE_NAME-${project.name}"
+    }
+
+    java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+    tasks.processResources {
+        filesMatching("META-INF/neoforge.mods.toml") {
+            expand(mapOf("version" to createVersionString()))
+        }
+    }
+
+    version = createVersionString()
+    group = "me.LoongLy.mods"
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release.set(21)
+    }
+
+    tasks.withType<GenerateModuleMetadata>().configureEach {
+        enabled = false
+    }
+}
+
+fun createVersionString(): String {
+    val builder = StringBuilder()
+
+    val isReleaseBuild = project.hasProperty("build.release")
+    val buildId = System.getenv("GITHUB_RUN_NUMBER")
+
+    if (isReleaseBuild) {
+        builder.append(MOD_VERSION)
+    } else {
+        builder.append(MOD_VERSION.split('-')[0])
+        builder.append("-snapshot")
+    }
+
+    builder.append("+mc").append(MINECRAFT_VERSION)
+
+    if (!isReleaseBuild) {
+        if (buildId != null) {
+            builder.append("-build.$buildId")
+        } else {
+            builder.append("-local")
+        }
+    }
+
+    return builder.toString()
+}
