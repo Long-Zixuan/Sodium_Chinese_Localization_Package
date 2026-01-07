adf34d24475a6d10dbaaa2fc7ded9fb65529e68e
Long-Zixuan <778275146@qq.com>
2026-01-07 14:30:22 +0800

    修改翻译   LZX-2025-01-07-001

--- common/src/main/java/me/loongly/mods/sclp/common/mixin/sodium/MixinPageBuilderImpl.java
+++ common/src/main/java/me/loongly/mods/sclp/common/mixin/sodium/MixinPageBuilderImpl.java
@@ -22,7 +22,7 @@ public class MixinPageBuilderImpl
     {
         if (name.getString().equals("General")) //这么屎山的玩意估计钠下一个版本就不会这样写了
         {
-            name = Component.translatable("sclp.options.pages.general");
+            name = Component.translatable("stat.generalButton");
             this.name = name;
         }
     }
