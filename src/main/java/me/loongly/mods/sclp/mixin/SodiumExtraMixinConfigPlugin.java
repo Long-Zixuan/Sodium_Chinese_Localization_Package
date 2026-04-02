18a98f5f9c8cc5dfdfe8bd9ff6235aa393c4ea21
Long-Zixuan <778275146@qq.com>
2026-04-02 12:21:57 +0800

    添加注入SodiumOptionAPI的逻辑   LZX-2026-04-02-002

--- src/main/java/me/loongly/mods/sclp/mixin/SodiumExtraMixinConfigPlugin.java
+++ src/main/java/me/loongly/mods/sclp/mixin/SodiumExtraMixinConfigPlugin.java
@@ -3,6 +3,10 @@ package me.loongly.mods.sclp.mixin;
 import me.loongly.mods.sclp.client.SCLPClientMod;
 import net.caffeinemc.caffeineconfig.AbstractCaffeineConfigMixinPlugin;
 import net.caffeinemc.caffeineconfig.CaffeineConfig;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
+
+import java.util.Optional;
 
 public class SodiumExtraMixinConfigPlugin extends AbstractCaffeineConfigMixinPlugin 
 {
@@ -17,5 +21,29 @@ public class SodiumExtraMixinConfigPlugin extends AbstractCaffeineConfigMixinPlu
     @Override
     protected String mixinPackageRoot() {
         return MIXIN_PACKAGE_ROOT;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) 
+    {
+        /*if(mixinClassName.equals("me.loongly.mods.sclp.mixin.compat.MixinSodiumOptionsAPIGUI")) 
+        {
+            try
+            {
+                if(ModList.get().getModContainerById("sodiumoptionsapi").isPresent())
+                {
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
+            } 
+            catch (Exception e)
+            {
+                return false;
+            }
+        }*/
+        return true;
     }
 }
