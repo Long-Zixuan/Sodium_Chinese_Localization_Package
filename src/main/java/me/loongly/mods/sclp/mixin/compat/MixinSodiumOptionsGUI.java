453b3463d8f0e90ee4db88a7f7bff0b168bcadfd
Long-Zixuan <778275146@qq.com>
2026-04-06 15:34:58 +0800

    充值文本翻译逻辑   LZX-2026-04-06-006

--- src/main/java/me/loongly/mods/sclp/mixin/compat/MixinSodiumOptionsGUI.java
+++ src/main/java/me/loongly/mods/sclp/mixin/compat/MixinSodiumOptionsGUI.java
@@ -14,6 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
+import net.minecraft.text.StringVisitable;
+import net.minecraft.text.Text;
+import net.minecraft.text.Style;
+
 @Mixin(value = SodiumOptionsGUI.class, remap = false)
 public class MixinSodiumOptionsGUI 
 {
@@ -26,5 +30,19 @@ public class MixinSodiumOptionsGUI
     private void init(CallbackInfo info)
     {
         this.pages.add(SCLPGameOptionPages.SCLPPage());
+    }
+
+      @Shadow
+    @Final
+    public static final List<StringVisitable> DONATION_PROMPT_MESSAGE;
+
+     static {
+        DONATION_PROMPT_MESSAGE = List.of(
+                StringVisitable.concat(Text.literal("您好！")),
+                StringVisitable.concat(Text.literal("看起来您十分中意"), Text.literal("Embeddium模组").setStyle(Style.EMPTY.withColor(0x27eb92)), Text.literal("，这是我的世界那钠模组的一个分支。")),
+                StringVisitable.concat(Text.literal("钠是十分复杂的，需要"), Text.literal("上千小时").setStyle(Style.EMPTY.withColor(0xff6e00)), Text.literal("来开发、测试，以达到玩家预期的效果。")),
+                StringVisitable.concat(Text.literal("如果您喜欢这个模组，不妨"), Text.literal("Support Sodium（支持钠）").setStyle(Style.EMPTY.withColor(0xed49ce)), Text.literal("。")),
+                StringVisitable.concat(Text.literal("最后再一次感谢您，希望这个模组可以帮助您（和您的电脑。）"))
+        );
     }
 }
