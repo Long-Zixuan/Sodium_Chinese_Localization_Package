01989be4135b914138a2bb9f52ec8631db402ef6
long-zixuan <778275146@qq.com>
2026-04-22 18:00:14 +0800

    I18NLanguage初始化提前  LZX-2026-04-22-009

--- src/main/java/me/loongly/mods/sclp/SCLPMod.java
+++ src/main/java/me/loongly/mods/sclp/SCLPMod.java
@@ -1,12 +1,15 @@
 package me.loongly.mods.sclp;
 
+import me.loongly.mods.sclp.language.I18NLanguage;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(SCLPMod.MOD_ID)
-public final class SCLPMod {
+public final class SCLPMod 
+{
     public static final String MOD_ID = "sclp";
 
     public SCLPMod()
     {
+        I18NLanguage.init();
     }
 }
