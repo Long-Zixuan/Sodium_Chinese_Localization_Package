971f5d3870542df26730f5c657a298fab2d33fb0
long-zixuan <778275146@qq.com>
2026-04-28 15:42:23 +0800

    翻译选项逻辑  LZX-2026-04-28-004

---
+++ src/main/java/me/loongly/mods/sclp/mixin/compat/MixinControlValueFormatter.java
@@ -0,0 +1,35 @@
+package me.loongly.mods.sclp.mixin.compat;
+
+import org.apache.logging.log4j.core.config.builder.api.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import net.minecraft.text.Text;
+import net.minecraft.client.resource.language.I18n;
+import me.loongly.mods.sclp.language.I18N;
+import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
+
+
+@Mixin(value = ControlValueFormatter.class,remap = false) 
+public interface MixinControlValueFormatter
+{
+    @Overwrite
+    static ControlValueFormatter quantityOrDisabled(String name, String disableText) 
+    {
+        String i18nName = I18N.trans(name);
+        String i18nDisableText = I18N.trans(disableText);
+        return (v) -> Text.literal(v == 0 ? i18nDisableText : v + " " + i18nName);
+    }
+
+    @Overwrite
+    static ControlValueFormatter multiplier() 
+    {
+        return (v) -> Text.literal(String.format("%s×",v));
+    }
+
+    @Overwrite
+    static ControlValueFormatter guiScale() 
+    {
+        return (v) -> (v == 0) ? Text.translatable("options.guiScale.auto") : Text.literal(String.format("%s×",v));
+    }
+}
