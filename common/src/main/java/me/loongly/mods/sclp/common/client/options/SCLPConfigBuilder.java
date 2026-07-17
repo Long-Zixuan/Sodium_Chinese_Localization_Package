a858d50d58cf3f0f14ebc029d49e03e5486ca9b8
Long-Zixuan <778275146@qq.com>
2026-07-18 02:31:36 +0800

    代码风格优化   LZX-2026-07-17-003

--- common/src/main/java/me/loongly/mods/sclp/common/client/options/SCLPConfigBuilder.java
+++ common/src/main/java/me/loongly/mods/sclp/common/client/options/SCLPConfigBuilder.java
@@ -3,7 +3,8 @@ package me.loongly.mods.sclp.common.client.options;
 import net.caffeinemc.mods.sodium.api.config.ConfigEntryPoint;
 import net.caffeinemc.mods.sodium.api.config.option.OptionFlag;
 import net.caffeinemc.mods.sodium.api.config.structure.ConfigBuilder;
-
+import net.caffeinemc.mods.sodium.api.config.structure.OptionGroupBuilder;
+import net.caffeinemc.mods.sodium.api.config.structure.OptionPageBuilder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.Util;
@@ -19,32 +20,46 @@ public class SCLPConfigBuilder implements ConfigEntryPoint
     @Override
     public void registerConfigLate(ConfigBuilder configBuilder) 
     {
-        configBuilder.registerOwnModOptions()
+        var modOpts = configBuilder.registerOwnModOptions()
                 .setColorTheme(configBuilder.createColorTheme()
                         .setBaseThemeRGB(0xed65ff)
                 )
                 .setIcon(Identifier.parse("sclp:texture/icon.png"))
-                .setVersion("5.4.2.1")
-                .addPage(configBuilder.createOptionPage()
-                        .setName(Component.translatable("sclp.pages.sclp_page.name"))
-                        .addOptionGroup(configBuilder.createOptionGroup()
+                .setVersion("5.4.2.1-Test");
+        var page = createOptionsPage(configBuilder);
+        page.addOptionGroup(createGeneralPage(configBuilder));
+        page.addOptionGroup(createSupportPage(configBuilder));
+        modOpts.addPage(page);
+    }
+
+    private OptionPageBuilder createOptionsPage(ConfigBuilder configBuilder) 
+    {
+        return configBuilder.createOptionPage()
+                        .setName(Component.translatable("sclp.pages.sclp_page.name"));
+    }
+
+    private OptionGroupBuilder createGeneralPage(ConfigBuilder configBuilder)
+    {
+        return configBuilder.createOptionGroup()
                                 .setName(Component.translatable("stat.generalButton"))
-                                .addOption(configBuilder.createBooleanOption(Identifier.parse("sclp:should_trans_mod_name"))//Builder(boolean.class, sclpOpts)
+                                .addOption(configBuilder.createBooleanOption(this.optionId("should_trans_mod_name"))//Builder(boolean.class, sclpOpts)
                                         .setName(Component.translatable("sclp.options.should_trans_mod_name.name"))
                                         .setTooltip(Component.translatable("sclp.options.should_trans_mod_name.tooltip"))
                                         .setBinding(value -> {sclpOpts.setShoudTransModName(value); SCLPClientMod.caiDan();}, () -> sclpOpts.getShouldTransModName())
                                         .setFlags(OptionFlag.REQUIRES_RENDERER_RELOAD)
                                         .setStorageHandler(sclpOpts::save)
                                         .setDefaultValue(SCLPOptions.DEFAULT_SHOULD_TRANS_MOD_NAME)
-                                )
-                        )
-                        .addOptionGroup(configBuilder.createOptionGroup()
+                                );
+    }
+
+    private OptionGroupBuilder createSupportPage(ConfigBuilder configBuilder)
+    {
+        return configBuilder.createOptionGroup()
                                 .setName(Component.translatable("sclp.options.group.support"))
                                 .addOption(configBuilder.createExternalButtonOption(this.optionId("support_project"))
                                         .setName(Component.translatable("sclp.options.support_project.name"))
                                         .setTooltip(Component.translatable("sclp.options.support_project.tooltip"))
-                                        .setScreenConsumer(screen -> Util.getPlatform().openUri("https://www.loongly.me/html/support_me_old.html"))))
-                );
+                                        .setScreenConsumer(screen -> Util.getPlatform().openUri("https://www.loongly.me/html/support_me_old.html")));
     }
 
     private Identifier optionId(String path) 
