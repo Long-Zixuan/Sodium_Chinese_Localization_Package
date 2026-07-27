3233d3ac254a72f202691a1f38f10a502aa43568
Long-Zixuan <778275146@qq.com>
2026-07-27 17:29:01 +0800

    Trans逻辑更改   LZX-2026-07-27-011

--- common/src/main/java/me/loongly/mods/sclp/common/language/I18N.java
+++ common/src/main/java/me/loongly/mods/sclp/common/language/I18N.java
@@ -14,7 +14,7 @@ public class I18N
         String languageCode = IPlatformHelper.INSTANCE.getLanguageCode();//I18n.get("sclp.cur_languagecode");
         Map<String, String> language = I18NLanguage.getInstance().getLanguage(languageCode);
         String string = key;
-        key = key.replace("\n", "<br>");//lang文件中换行统一用<br>
+
         if(language != null && language.containsKey(key))
         {
             string = language.get(key);
@@ -23,7 +23,7 @@ public class I18N
         {
             return I18n.get(key, args);
         }
-        string = string.replace("<br>", "\n");//lang文件中换行统一用<br>
+
         try 
         {
             return String.format(string, args);
@@ -39,7 +39,7 @@ public class I18N
         String languageCode = IPlatformHelper.INSTANCE.getLanguageCode();
         Map<?, ?> language = I18NLanguage.getInstance().getLanguage(languageCode);
         Map<?, ?> fallbackLanguage = I18NLanguage.getInstance().getLanguage("en_us");
-        key = key.replace("\n", "<br>");//键值的换行统一用<br>代替
+        
         if(language != null && language.containsKey(key))
         {
             return 2;
