7dcd61a2d1a1f960c7f11faee296b7866ba85fca
Long-Zixuan <778275146@qq.com>
2026-08-07 01:20:44 +0800

    LZX-2026-08-06-005

--- common/src/main/java/me/loongly/mods/sclp/common/mixin/sodium/MixinScreenPrompt.java
+++ common/src/main/java/me/loongly/mods/sclp/common/mixin/sodium/MixinScreenPrompt.java
@@ -1,31 +1,13 @@
 package me.loongly.mods.sclp.common.mixin.sodium;
-import net.caffeinemc.mods.sodium.client.SodiumClientMod;
 
-import net.caffeinemc.mods.sodium.client.config.builder.PageBuilderImpl;
 import net.minecraft.network.chat.Component;
-
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.List;
 
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import me.loongly.mods.sclp.common.client.SCLPClientMod;
-import net.caffeinemc.mods.sodium.client.gui.VideoSettingsScreen;
 import net.caffeinemc.mods.sodium.client.gui.widgets.FlatButtonWidget;
 import net.caffeinemc.mods.sodium.client.util.Dim2i;
-import net.minecraft.network.chat.FormattedText;
-import net.minecraft.Util;
-import net.caffeinemc.mods.sodium.client.gui.ButtonTheme;
-import net.caffeinemc.mods.sodium.client.gui.Colors;
-import net.caffeinemc.mods.sodium.client.gui.SodiumOptions;
 import net.caffeinemc.mods.sodium.client.gui.prompt.ScreenPrompt;
 import net.caffeinemc.mods.sodium.client.gui.prompt.ScreenPromptable;
 
@@ -45,12 +27,10 @@ public class MixinScreenPrompt
     @Shadow
     FlatButtonWidget closeButton;
 
-    private static final ButtonTheme PROMPT_THEME = new ButtonTheme(Colors.FOREGROUND, Colors.FOREGROUND, Colors.FOREGROUND, 0xff393939, 0xff2b2b2b, 0xff2b2b2b);
-
     @Inject(method = "init", at = @At(value = "RETURN"))
     public void injectInit(CallbackInfo c)
     {
-        var scrIns = ((ScreenPrompt)(Object)this);
+        //var scrIns = ((ScreenPrompt)(Object)this);
 
         var parentDimensions = this.parent.getDimensions();
 
@@ -64,6 +44,17 @@ public class MixinScreenPrompt
     {
         this.parent.setPrompt(null);
     }
+
+    private static FlatButtonWidget.Style createButtonStyle() 
+    {
+      FlatButtonWidget.Style style = new FlatButtonWidget.Style();
+      style.bgDefault = -13948117;
+      style.bgHovered = -13027015;
+      style.bgDisabled = style.bgDefault;
+      style.textDefault = -1;
+      style.textDisabled = style.textDefault;
+      return style;
+   }
 }
 
 //LZX-2026-04-03-001
