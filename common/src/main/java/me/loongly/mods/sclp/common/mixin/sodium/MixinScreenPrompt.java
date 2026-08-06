8af4948d313552e0cf865792871d11262fbd6228
Long-Zixuan <778275146@qq.com>
2026-08-07 03:37:48 +0800

    导致Sodium赞助页面崩溃问题解决   LZX-2026-08-07-002

--- common/src/main/java/me/loongly/mods/sclp/common/mixin/sodium/MixinScreenPrompt.java
+++ common/src/main/java/me/loongly/mods/sclp/common/mixin/sodium/MixinScreenPrompt.java
@@ -21,57 +21,24 @@ import java.util.List;
 @Mixin(ScreenPrompt.class)
 public abstract class MixinScreenPrompt//不继承ScreenPrompt，原因未知（具体是public @NotNull List<AbstractWidget> getWidgets()函数报错了），但是继承了之后对init的overwrite不起作用，原因未知
 {
-    @Final
-    @Shadow
-    private int width, height;
-   
-    @Shadow
+    @Shadow @Final
     FlatButtonWidget closeButton;
 
-    @Shadow
-    FlatButtonWidget actionButton;
-    @Final
-    @Shadow
-    ScreenPromptable parent;
-
-    @Final
-    @Shadow
-    Action action;
-
-    @Overwrite
-    public void init() 
+    @Inject(method = "init", at = @At(value = "RETURN"))
+    public void injectInit(CallbackInfo c)
     {
-      Dim2i parentDimensions = this.parent.getDimensions();
-      int boxX = parentDimensions.width() / 2 - this.width / 2;
-      int boxY = parentDimensions.height() / 2 - this.height / 2;
-      this.closeButton = new FlatButtonWidget(new Dim2i(boxX + this.width - 84, boxY + this.height - 24, 80, 20), Component.translatable("sclp.close"), this::close);
-      this.closeButton.setStyle(createButtonStyle());
-
-      this.actionButton = new FlatButtonWidget(new Dim2i(boxX + this.width - 198, boxY + this.height - 24, 110, 20), this.action.label(), this::runAction);
-      this.actionButton.setStyle(createButtonStyle());
-   }
-
-    private void close() 
-    {
-        this.parent.setPrompt(null);
+      Class<?> clazz = closeButton.getClass();
+      try 
+      {
+        Field field = clazz.getDeclaredField("label");
+        field.setAccessible(true);
+        field.set(closeButton, Component.literal(I18N.trans("sclp.close")));
+      }
+      catch (Exception e) 
+      {
+        e.printStackTrace();
+      }
     }
-
-    private void runAction() 
-    {
-      this.action.runnable().run();
-      this.close();
-    }
-
-    private static FlatButtonWidget.Style createButtonStyle() 
-    {
-      FlatButtonWidget.Style style = new FlatButtonWidget.Style();
-      style.bgDefault = -13948117;
-      style.bgHovered = -13027015;
-      style.bgDisabled = style.bgDefault;
-      style.textDefault = -1;
-      style.textDisabled = style.textDefault;
-      return style;
-   }
 }
 
 //LZX-2026-04-03-001
