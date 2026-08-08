72dbedc80bbb77e159338cf44c97b93171210f2c
Long-Zixuan <778275146@qq.com>
2026-08-08 21:26:08 +0800

    RSO环境配置  LZX-2026-08-08-002

---
+++ common/src/main/java/me/loongly/mods/sclp/common/mixin/reeses_sodium_options/MixinRSOVideoScreen.java
@@ -0,0 +1,10 @@
+package me.loongly.mods.sclp.common.mixin.reeses_sodium_options;
+import org.spongepowered.asm.mixin.Mixin;
+
+import me.flashyreese.mods.reeses_sodium_options.client.gui.SodiumVideoOptionsScreen;
+
+@Mixin(SodiumVideoOptionsScreen.class)
+public class MixinRSOVideoScreen 
+{
+    
+}
