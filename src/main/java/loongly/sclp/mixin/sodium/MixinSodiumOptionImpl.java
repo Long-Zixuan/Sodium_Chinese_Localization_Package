b3cf3605995934d5ab17c1dffd806e23c276f581
Long-Zixuan <778275146@qq.com>
2026-08-09 03:57:13 +0800

    合并文件   LZX-2026-08-08-014

--- src/main/java/loongly/sclp/mixin/sodium/MixinSodiumOptionImpl.java
+++ src/main/java/loongly/sclp/mixin/sodium/MixinSodiumOptionImpl.java
@@ -1,15 +1,22 @@
 package loongly.sclp.mixin.sodium;
 import me.jellysquid.mods.sodium.client.gui.options.OptionImpl;
+import loongly.sclp.client.SclpClientMod;
 import loongly.sclp.language.I18N;
 //import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+
+import java.util.HashMap;
+
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(OptionImpl.class)
+@Mixin(value = OptionImpl.class,remap = false)
 public class MixinSodiumOptionImpl
 {
     @Inject(method = "getName", at = @At(value = "RETURN",target = "Lnet/minecraft/client/resource/language/I18n;translate(Ljava/lang/String;)V"), cancellable = true)
@@ -27,5 +34,22 @@ public class MixinSodiumOptionImpl
         String tranTipStr = I18N.trans(oriTipStr);
         Text tranTip = new LiteralText(tranTipStr);
         cir.setReturnValue(tranTip);
+    }
+
+    HashMap<String,Runnable> sclpEvents = new HashMap<String,Runnable>(){{
+        put("sclp.support",()->{SclpClientMod.openSupportPage();});
+        put("sclp.birth",()->{SclpClientMod.birthCaidan();});
+    }};
+
+    @Shadow @Final
+    String name;
+    @Inject(method = "setValue", at = @At("HEAD"), cancellable = true)
+    private void injectSetValue(Object value, CallbackInfo ci) 
+    {
+        if(sclpEvents.containsKey(name))
+        {
+            sclpEvents.get(name).run();
+            ci.cancel();
+        }
     }
 }
