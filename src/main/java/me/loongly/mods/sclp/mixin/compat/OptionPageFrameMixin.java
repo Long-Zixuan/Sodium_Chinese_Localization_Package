a3c01895ca70963c56604aa6660d622f335d4040
Long-Zixuan <778275146@qq.com>
2026-08-12 22:55:15 +0800

    icon更改+版本号更改   LZX-2026-08-11-006

--- src/main/java/me/loongly/mods/sclp/mixin/compat/OptionPageFrameMixin.java
+++ src/main/java/me/loongly/mods/sclp/mixin/compat/OptionPageFrameMixin.java
@@ -57,7 +57,7 @@ class OptionPageFrameMixin
                 sclpScreen.open();
             }
             //drawContext.drawTexture(LOGO_LOCATION, mouseX - LOGO_SIZE / 2, mouseY - LOGO_SIZE / 2, 0, 0, LOGO_SIZE, LOGO_SIZE, LOGO_SIZE, LOGO_SIZE);
-            drawContext.drawTexture(LS_LOCATION, 0, 0, 0, 0, 36, 20, 36, 20);
+            drawContext.drawTexture(LS_LOCATION, 0, 0, 0, 0, 40, 22, 40, 22);
             return;
         }
         var embScreen = getEmbScreen();
