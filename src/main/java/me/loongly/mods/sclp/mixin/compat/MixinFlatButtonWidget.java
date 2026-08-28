2eae7885a6f5ac8200edbdacdd5f7f4ff6e53088
Long-Zixuan <778275146@qq.com>
2026-08-29 02:01:26 +0800

    1.16通用Rubidium按钮翻译逻辑(上个不行有问题)   LZX-2026-08-28-013

--- src/main/java/me/loongly/mods/sclp/mixin/compat/MixinFlatButtonWidget.java
+++ src/main/java/me/loongly/mods/sclp/mixin/compat/MixinFlatButtonWidget.java
@@ -8,36 +8,69 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.lang.reflect.Field;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-@Mixin(value = FlatButtonWidget.class,remap = false)
-public class MixinFlatButtonWidget
+@Mixin(FlatButtonWidget.class)
+public class MixinFlatButtonWidget//1.16.5的高版本铷，构造函数label是ITextComponent类型，所以不兼容，故屏蔽
 {
-    /*@ModifyArg(
-    method = "<init>(Lme/jellysquid/mods/sodium/client/util/Dim2i;Ljava/lang/String;Ljava/lang/Runnable;)V",
-        at = @At(value = "INVOKE", 
-                target = "java/lang/String", 
-                ordinal = 0),
-        index = 1
-    )
-    private String modifyLabel(String originalLabel) 
+    @Inject(method = "<init>", at = @At(value = "RETURN"))
+    public void InjectInit(Dim2i dim, String label, Runnable action, CallbackInfo ci)
     {
-        return I18N.trans(originalLabel);
-    }*/
-   @Redirect(
-        method = "<init>(Lme/jellysquid/mods/sodium/client/util/Dim2i;Ljava/lang/String;Ljava/lang/Runnable;)V",
-        at = @At(value = "INVOKE", 
-                target = "java/lang/String", 
-                ordinal = 0),
-        require = 0 // 表示不依赖其他条件，总是尝试注入
-    )
-    private String redirectLabel(String originalLabel) 
-    {
-        return I18N.trans(originalLabel);
+        try 
+        {
+            Field field = FlatButtonWidget.class.getDeclaredField("label");
+            field.setAccessible(true);
+            Object l = field.get((FlatButtonWidget)(Object)this);
+            if(l instanceof String)
+            {
+                field.set((FlatButtonWidget)(Object)this, I18N.trans(label));
+            }
+            else
+            {
+                Class<?> clazz;
+            try 
+            {
+                clazz = Class.forName("net.minecraft.util.text.TranslationTextComponent");//这玩意继承了ITextComponent
+            } 
+            catch (ClassNotFoundException e) 
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return;
+            }
+            Constructor<?> constructor;
+            try 
+            {
+                constructor = clazz.getConstructor(String.class, Object[].class);
+            } 
+            catch (NoSuchMethodException | SecurityException e) 
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                    return;
+            }
+            Object translationTextComponent;
+            try 
+            {
+                translationTextComponent = constructor.newInstance(label, null);
+            } 
+            catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException e) 
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                    return;
+            }
+                field.set((FlatButtonWidget)(Object)this, translationTextComponent);
+            }
+        }
+        catch (NoSuchFieldException | IllegalAccessException e) 
+        {
+            e.printStackTrace();
+        }
     }
 }
