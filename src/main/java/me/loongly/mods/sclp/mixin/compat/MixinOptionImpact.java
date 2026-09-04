fc88557cc902661773fe42eebf7efe0f9b5b1ec9
Long-Zixuan <778275146@qq.com>
2026-09-04 16:47:10 +0800

    影响程度翻译逻辑   LZX-2026-09-04-001

---
+++ src/main/java/me/loongly/mods/sclp/mixin/compat/MixinOptionImpact.java
@@ -0,0 +1,49 @@
+package me.loongly.mods.sclp.mixin.compat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.lang.reflect.Field;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Shadow;
+
+import me.loongly.mods.sclp.language.I18N;
+
+import me.jellysquid.mods.sodium.client.gui.options.OptionImpact;
+
+import net.minecraft.text.Text;
+import me.loongly.mods.sclp.client.SCLPClientMod;
+import me.loongly.mods.sclp.client.gui.ViaOpt;
+
+@Mixin(value = OptionImpact.class, remap = false)
+public class MixinOptionImpact 
+{
+    #if BEFORE_18_1
+    @Inject(method = "<init>", at = @At("RETURN"),cancellable = true)
+    private void injectInit(CallbackInfo c)
+    {
+        try
+        {
+            Field field = OptionImpact.class.getDeclaredField("text");
+            field.setAccessible(true);
+            Object t = field.get((OptionImpact)(Object)this);
+            if(t instanceof String)
+            {
+                field.set((OptionImpact)(Object)this, I18N.trans((String)t));
+            }
+        }
+        catch (NoSuchFieldException | IllegalAccessException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    #endif
+}    
