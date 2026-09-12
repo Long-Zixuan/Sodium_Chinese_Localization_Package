c75a72b7eb26699ee98a9c8a0e3e0c4babc608ad
Long-Zixuan <778275146@qq.com>
2026-09-12 17:09:30 +0800

    优化   LZX-2026-09-12-002

--- src/main/java/me/loongly/mods/sclp/client/Environment.java
+++ src/main/java/me/loongly/mods/sclp/client/Environment.java
@@ -14,142 +14,165 @@ import me.jellysquid.mods.sodium.client.gui.options.Option;
 public class Environment 
 {
     #if BEFORE_18_1
-    public static Class<?> textCompClazz_1_16 = null;
-    public static Constructor<?> textCompClazz_1_16_Constructor = null;
-    public static Class<?> iTextCompClazz_1_16 = null;
-    public static Constructor<?> cyclingControlClz_1_16_ConOCI = null;
+    public static final Class<?> textCompClazz_1_16;
+    public static final Constructor<?> textCompClazz_1_16_Constructor;
+    public static final Class<?> iTextCompClazz_1_16;
+    public static final Constructor<?> cyclingControlClz_1_16_ConOCI;
     #else
-    public static Class<?> textCompClazz_1_19 = null;
-    public static Method textCompClazz_1_19_Constructor = null;
+    public static final Class<?> textCompClazz_1_19;
+    public static final Method textCompClazz_1_19_Constructor;
     #endif
-    public static Class<?> optionPageClazz = null;
-    public static Constructor<?> optionPageConstructor = null;
-    public static Constructor<?> cyclingControlClzCon = null;
+    public static final Class<?> optionPageClazz;
+    public static final Constructor<?> optionPageConstructor;
+    public static final Constructor<?> cyclingControlClzCon;
 
     static
     {    
         #if BEFORE_18_1
+        Class<?> tempTextCompClazz_1_16 = null;
+        Constructor<?> tempTextCompClazz_1_16_Constructor = null;
+        Class<?> tempITextCompClazz_1_16 = null;
+        Constructor<?> tempCyclingControlClz_1_16_ConOCI = null;
+        
         try 
         {
-            textCompClazz_1_16 = Class.forName("net.minecraft.util.text.TranslationTextComponent");//这玩意继承了ITextComponent
+            tempTextCompClazz_1_16 = Class.forName("net.minecraft.util.text.TranslationTextComponent");
         } 
         catch (ClassNotFoundException e) 
         {
-            // TODO Auto-generated catch block
-            SCLPClientMod.logger().error("[SCLP] :",e);//warn
+            SCLPClientMod.logger().error("[SCLP] :",e);
             throw new RuntimeException("[SCLP]" + e.toString());
         }
         
         try 
         {
-            textCompClazz_1_16_Constructor = textCompClazz_1_16.getConstructor(String.class, Object[].class);
+            tempTextCompClazz_1_16_Constructor = tempTextCompClazz_1_16.getConstructor(String.class, Object[].class);
         } 
         catch (NoSuchMethodException | SecurityException e) 
         {
-            // TODO Auto-generated catch block
-            SCLPClientMod.logger().error("[SCLP] :",e);//warn
+            SCLPClientMod.logger().error("[SCLP] :",e);
             throw new RuntimeException("[SCLP]" + e.toString());
         }
         try 
         {
-            iTextCompClazz_1_16 = Class.forName("net.minecraft.util.text.ITextComponent");
+            tempITextCompClazz_1_16 = Class.forName("net.minecraft.util.text.ITextComponent");
         } 
         catch (ClassNotFoundException e) 
         {
-            // TODO Auto-generated catch block
-            SCLPClientMod.logger().error("[SCLP] :",e);//warn
+            SCLPClientMod.logger().error("[SCLP] :",e);
             throw new RuntimeException("[SCLP]" + e.toString());
         }
+        
         #else
+        Class<?> tempTextCompClazz_1_19 = null;
+        Method tempTextCompClazz_1_19_Constructor = null;
+        
         try
         {
-            textCompClazz_1_19 = Class.forName("net.minecraft.network.chat.Component");
+            tempTextCompClazz_1_19 = Class.forName("net.minecraft.network.chat.Component");
         }
         catch (ClassNotFoundException e)
         {
-            SCLPClientMod.logger().error("[SCLP] If you game version is 1.18,Please ignore this error:",e);//warn
+            SCLPClientMod.logger().error("[SCLP] If you game version is 1.18,Please ignore this error:",e);
         }
         
         try
         {
-            if(textCompClazz_1_19 != null)
+            if(tempTextCompClazz_1_19 != null)
             {
-                textCompClazz_1_19_Constructor = textCompClazz_1_19.getMethod("m_237115_",String.class);//m_237115_是translatable的方法名
+                tempTextCompClazz_1_19_Constructor = tempTextCompClazz_1_19.getMethod("m_237115_",String.class);
             }
         } 
         catch (NoSuchMethodException | SecurityException e) 
         {
-            // TODO Auto-generated catch block
-            SCLPClientMod.logger().error("[SCLP] :",e);//warn
+            SCLPClientMod.logger().error("[SCLP] :",e);
         }
+        
+        textCompClazz_1_19 = tempTextCompClazz_1_19;
+        textCompClazz_1_19_Constructor = tempTextCompClazz_1_19_Constructor;
         #endif
        
+        Class<?> tempOptionPageClazz = null;
+        Constructor<?> tempOptionPageConstructor = null;
+        Constructor<?> tempCyclingControlClzCon = null;
+        
         try
         {
-            optionPageClazz = Class.forName("me.jellysquid.mods.sodium.client.gui.options.OptionPage");
+            tempOptionPageClazz = Class.forName("me.jellysquid.mods.sodium.client.gui.options.OptionPage");
         }
         catch (ClassNotFoundException e)
         {
-            SCLPClientMod.logger().error("[SCLP] :",e);//warn
+            SCLPClientMod.logger().error("[SCLP] :",e);
         }
 
         try 
         {
             #if BEFORE_18_1
-            optionPageConstructor = optionPageClazz.getConstructor(String.class, com.google.common.collect.ImmutableList.class);
+            if(tempOptionPageClazz != null) {
+                tempOptionPageConstructor = tempOptionPageClazz.getConstructor(String.class, com.google.common.collect.ImmutableList.class);
+            }
             #else
-            optionPageConstructor = optionPageClazz.getConstructor(textCompClazz_1_19, com.google.common.collect.ImmutableList.class);
+            if(tempOptionPageClazz != null && textCompClazz_1_19 != null) {
+                tempOptionPageConstructor = tempOptionPageClazz.getConstructor(textCompClazz_1_19, com.google.common.collect.ImmutableList.class);
+            }
             #endif
         } 
         catch (NoSuchMethodException | SecurityException e) 
         {
-            // TODO Auto-generated catch block
-            SCLPClientMod.logger().error("[SCLP] If you game version is 1.18,Please ignore this error :",e);//warn
+            SCLPClientMod.logger().error("[SCLP] If you game version is 1.18,Please ignore this error :",e);
         }
+        
         try
         {
             #if BEFORE_18_1
-            cyclingControlClzCon = CyclingControl.class.getConstructor(
-                Option.class,      // 对应Option<T>
-                Class.class,       // 对应Class<T>
-                String[].class     // 对应String[]
+            tempCyclingControlClzCon = CyclingControl.class.getConstructor(
+                Option.class,      
+                Class.class,       
+                String[].class     
             );
             #else
             if(textCompClazz_1_19 != null)
             {
                 Class<?> textCompArrayClazz = textCompClazz_1_19.arrayType();
-                cyclingControlClzCon = CyclingControl.class.getConstructor(
-                    Option.class,      // 对应Option<T>
-                    Class.class,       // 对应Class<T>
-                    textCompArrayClazz     // 对应Component[]
+                tempCyclingControlClzCon = CyclingControl.class.getConstructor(
+                    Option.class,      
+                    Class.class,       
+                    textCompArrayClazz     
                 );
             }
             #endif
         }
         catch (NoSuchMethodException | IllegalArgumentException e)
         {
-            SCLPClientMod.logger().error("[SCLP] If you are using rubiddium and the version is 0.2.13 please ignore this error:",e);//warn
+            SCLPClientMod.logger().error("[SCLP] If you are using rubiddium and the version is 0.2.13 please ignore this error:",e);
         }
 
         #if BEFORE_18_1
-        if(cyclingControlClzCon == null)
+        if(tempCyclingControlClzCon == null && tempITextCompClazz_1_16 != null)
         {
-            Class<?> iTextCmpClazzArr = iTextCompClazz_1_16.arrayType();
+            Class<?> iTextCmpClazzArr = tempITextCompClazz_1_16.arrayType();
             try 
             {
-                cyclingControlClz_1_16_ConOCI = CyclingControl.class.getConstructor(
-                    Option.class,      // 对应Option<T>
-                    Class.class,       // 对应Class<T>
+                tempCyclingControlClz_1_16_ConOCI = CyclingControl.class.getConstructor(
+                    Option.class,      
+                    Class.class,       
                     iTextCmpClazzArr
                 );
             } 
             catch ( NoSuchMethodException | SecurityException e) 
             {
-                // TODO Auto-generated catch block
-                SCLPClientMod.logger().error("[SCLP] :",e);//warn
+                SCLPClientMod.logger().error("[SCLP] :",e);
                 throw new RuntimeException("[SCLP]" + e.toString());
             }
         }
+        textCompClazz_1_16 = tempTextCompClazz_1_16;
+        textCompClazz_1_16_Constructor = tempTextCompClazz_1_16_Constructor;
+        iTextCompClazz_1_16 = tempITextCompClazz_1_16;
+        cyclingControlClz_1_16_ConOCI = tempCyclingControlClz_1_16_ConOCI;
         #endif
+        
+        optionPageClazz = tempOptionPageClazz;
+        optionPageConstructor = tempOptionPageConstructor;
+        cyclingControlClzCon = tempCyclingControlClzCon;
     }
 }
