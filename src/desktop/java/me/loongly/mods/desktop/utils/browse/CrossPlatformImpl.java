84f1afbb21f88e92695866d6a8c2548fb433446e
Long-Zixuan <778275146@qq.com>
2026-09-19 02:49:03 +0800

    添加非法启动警告   2026-09-19-006

---
+++ src/desktop/java/me/loongly/mods/desktop/utils/browse/CrossPlatformImpl.java
@@ -0,0 +1,20 @@
+package me.loongly.mods.sclp.desktop.utils.browse;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
+class CrossPlatformImpl implements BrowseUrlHandler 
+{
+    public static boolean isSupported() 
+    {
+        return Desktop.getDesktop()
+                .isSupported(Desktop.Action.BROWSE);
+    }
+
+    @Override
+    public void browseTo(String url) throws IOException 
+    {
+        Desktop.getDesktop().browse(URI.create(url));
+    }
+}
